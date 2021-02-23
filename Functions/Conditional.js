b072d4ad74a3c45ab4839346b4cb316de256fd08
scbonner <45452011+scbonner@users.noreply.github.com>
2021-02-23 10:11:02 -0500

    additional files

--- Functions/Conditional.js
+++ Functions/Conditional.js
@@ -25,3 +25,18 @@ test(false); // returns "It was false"
 
 // Create an if statement inside the function to return "Yes, that was true" if the parameter wasThatTrue is true and return "No, that was false" otherwise.
 
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+    if (wasThatTrue) {
+      return 'Yes, that was true'
+    }
+      return "No, that was false"
+  
+    // Only change code above this line
+    }
+  // calling or invoking the function
+  // pay attention to placement
+ 
+trueOrFalse(true);
+trueOrFalse(false);
+
