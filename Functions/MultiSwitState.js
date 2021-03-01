79d11e4b903a1970d4c3bb284943165a27eff915
scbonner <45452011+scbonner@users.noreply.github.com>
2021-02-28 19:52:05 -0500

    adding updates

--- Functions/MultiSwitState.js
+++ Functions/MultiSwitState.js
@@ -11,12 +11,39 @@ switch(val) {
   case 4:
     result = "4 alone";
 }
-Cases for 1, 2, and 3 will all produce the same result.
+// Cases for 1, 2, and 3 will all produce the same result.
 
-Write a switch statement to set answer for the following ranges:
+// Write a switch statement to set answer for the following ranges:
 1-3 - "Low"
 4-6 - "Mid"
 7-9 - "High"
 
-Note
-You will need to have a case statement for each number in the range.
+// Note
+// You will need to have a case statement for each number in the range.
+
+function sequentialSizes(val) {
+    var answer = "";
+    // Only change code below this line
+  
+  switch (val) {
+      case 1:
+      case 2:
+      case 3:
+        return "Low";
+        break;
+      case 4:
+      case 5:
+      case 6:
+        return "Mid";
+        break;
+      case 7:
+      case 8:
+      case 9:
+        return "High";
+        break;
+    }
+    // Only change code above this line
+    return answer;
+  }
+  // Change this value to test
+  sequentialSizes(1);
